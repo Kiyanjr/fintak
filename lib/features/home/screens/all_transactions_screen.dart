42537b39c9049ac6216d238a27c84ce9cc7a8ace
Kiyan jr <kiyanjohari81@gmail.com>
2026-07-12 16:43:24 +0330

    [feat]: amount tile in home is added

---
+++ lib/features/home/screens/all_transactions_screen.dart
@@ -0,0 +1,37 @@
+
+import 'package:fintak/core/constants/app_colors.dart';
+import 'package:fintak/features/home/viewmodels/home_viewmodel.dart';
+import 'package:fintak/features/home/widgets/transaction_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AllTransactionsScreen extends ConsumerWidget {
+  const AllTransactionsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Watch the home view model state to get the full list of transactions
+    final homeState=ref.watch(homeViewmodelProvider);
+    final transactions=homeState.transactions;
+    return Scaffold(
+      appBar:AppBar(
+      title:  const Text('All Transactions'),
+      centerTitle: true,
+    ),
+    body: transactions.isEmpty
+    ? Center(
+      child: Text(
+                'No transactions found',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.accent,
+                ),
+              ),
+    )
+    : TransactionList(transactions: transactions) ,
+    );
+
+  }
+}
